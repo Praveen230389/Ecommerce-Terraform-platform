43a6e39b87bd5f8239c5bda21d138889cb0ac1af
Praveen230389 <Praveenkumar230389@gmail.com>
2026-07-02 10:42:52 +0530

    Update installsoftwares.tf

--- terraform/installsoftwares.tf
+++ terraform/installsoftwares.tf
@@ -1,21 +1,18 @@
 ################################
 # JENKINS + ANSIBLE SERVER
 ################################
-
 resource "aws_instance" "jenkins" {
-
   ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = "t3.medium"
+  subnet_id     = aws_subnet.public1.id
 
-  subnet_id = aws_subnet.public1.id
-
+  # Updated to use the secure isolated Jenkins Security Group
   vpc_security_group_ids = [
-    aws_security_group.main.id
+    aws_security_group.jenkins_sg.id
   ]
 
   iam_instance_profile = aws_iam_instance_profile.main.name
-
-  user_data = file("scripts/jenkins.sh")
+  user_data            = file("scripts/jenkins.sh")
 
   tags = {
     Name = "jenkins-server"
@@ -23,18 +20,16 @@ resource "aws_instance" "jenkins" {
 }
 
 ################################
-# DEVSECOPS SERVER
+# DEVSECOPS SERVER (SonarQube & Nexus)
 ################################
-
 resource "aws_instance" "devsecops" {
-
   ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = "t3.large"
+  subnet_id     = aws_subnet.public1.id
 
-  subnet_id = aws_subnet.public1.id
-
+  # Updated to use the secure DevSecOps Security Group
   vpc_security_group_ids = [
-    aws_security_group.main.id
+    aws_security_group.devsecops_sg.id
   ]
 
   user_data = file("scripts/devsecops.sh")
@@ -45,18 +40,16 @@ resource "aws_instance" "devsecops" {
 }
 
 ################################
-# MONITORING SERVER
+# MONITORING SERVER (Prometheus & Grafana)
 ################################
-
 resource "aws_instance" "monitoring" {
-
   ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = "t3.medium"
+  subnet_id     = aws_subnet.public1.id
 
-  subnet_id = aws_subnet.public1.id
-
+  # Updated to use the isolated Monitoring Security Group
   vpc_security_group_ids = [
-    aws_security_group.main.id
+    aws_security_group.monitoring_sg.id
   ]
 
   user_data = file("scripts/monitoring.sh")
@@ -65,4 +58,3 @@ resource "aws_instance" "monitoring" {
     Name = "monitoring-server"
   }
 }
-
