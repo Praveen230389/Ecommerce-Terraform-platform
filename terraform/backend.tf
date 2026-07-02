d56b2baecca95a4fb07024718aa0442843c6b62b
Praveen230389 <Praveenkumar230389@gmail.com>
2026-07-02 12:19:54 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,11 +4,13 @@
 
 terraform {
   backend "s3" {
-    bucket         = "ecommerce-terraform-state-bucket-12345"
+    # FIXED: यहाँ अपने एडब्ल्यूएस पर बनाए गए असली बकेट का नाम डालें
+    bucket         = "praveen-tfstate-bucket-unique-99" 
     key            = "prod/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock-table"
+    
+    # FIXED: वॉर्निंग हटाने के लिए पुरानी dynamodb_table को आधुनिक use_lockfile से बदल दिया
+    use_lockfile   = true
   }
 }
-
